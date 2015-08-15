fa5cf3b9a05541d41d7db7095db3414923dc67c2
eljam <guillaume.cavana@gmail.com>
2015-08-15 19:35:19 +0200

    Add home props

--- app/components/home.jsx
+++ app/components/home.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 
 export default class Home extends React.Component {
-    render () {
-        return <h1>{this.props.name}</h1>
-    }
+
+    render() {
+        return <h1>{this.props.name}</h1>;
+      }
 }
+
+Home.propTypes = { name: React.PropTypes.string.isRequired };
